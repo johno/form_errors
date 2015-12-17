5e0aaf0cbf8fb9f1315982240bc0c3be7d06e7db
John Otander <johnotander@gmail.com>
2015-12-16 20:16:14 -0700

    Add basscss error display, 1.1.0

--- lib/form_errors/view_helpers.rb
+++ lib/form_errors/view_helpers.rb
@@ -19,12 +19,30 @@ module FormErrors
       .html_safe
     end
 
+    def bass_display_errors(object, error_string = nil, html = {})
+      return unless object && object.errors.any?
+
+      <<-HTML
+        <div class="bold p2 white bg-red">
+          <h2 class="#{ html[:error_string_class] }">
+            #{ get_error_string(error_string, object) }
+          </h2>
+          <ul class="#{ html[:error_list_class] }">
+            #{ object.errors.full_messages.map do |msg|
+                 "<li class=\"#{ html[:error_list_item_class] }\">#{ msg }</li>"
+               end.join }
+          </ul>
+        </div>
+      HTML
+      .html_safe
+    end
+
     private
 
-      def get_error_string(error_string, object)
-        error_string || "#{ pluralize(object.errors.count, "error") } "\
-                        "prohibited this #{ object.class.name.underscore.humanize.downcase } "\
-                        "from being saved"
-      end
+    def get_error_string(error_string, object)
+      error_string || "#{ pluralize(object.errors.count, "error") } "\
+                      "prohibited this #{ object.class.name.underscore.humanize.downcase } "\
+                      "from being saved"
+    end
   end
 end
